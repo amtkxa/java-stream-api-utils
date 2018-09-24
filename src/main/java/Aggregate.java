e32a1b9a80a7604f6045f87dcc6089763e63e29a
amtkxa <scarlet0211@outlook.jp>
2018-09-25 00:34:20 +0900

    max, average を追加

--- src/main/java/Aggregate.java
+++ src/main/java/Aggregate.java
@@ -1,6 +1,7 @@
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
 
 /**
  * 集約操作
@@ -17,5 +18,27 @@ public class Aggregate {
     public static Integer min(List<Integer> arrays) {
         Optional<Integer> min = arrays.stream().min(Comparator.naturalOrder());
         return min.get();
+    }
+
+    /**
+     * 最大値の取得
+     *
+     * @param arrays
+     * @return 最大値
+     */
+    public static Integer max(List<Integer> arrays) {
+        Optional<Integer> max = arrays.stream().max(Comparator.naturalOrder());
+        return max.get();
+    }
+
+    /**
+     * 平均値の取得
+     *
+     * @param arrays
+     * @return 平均値
+     */
+    public static Double average(List<Integer> arrays) {
+        OptionalDouble average = arrays.stream().mapToDouble(num -> num).average();
+        return average.getAsDouble();
     }
 }
--- src/test/java/AggregateTest.java
+++ src/test/java/AggregateTest.java
@@ -13,4 +13,18 @@ class AggregateTest {
         Integer result = Aggregate.min(arrays);
         assertThat(result, is(100));
     }
+
+    @Test
+    void max() {
+        List<Integer> arrays = Arrays.asList(200, 100, 300);
+        Integer result = Aggregate.max(arrays);
+        assertThat(result, is(300));
+    }
+
+    @Test
+    void average() {
+        List<Integer> arrays = Arrays.asList(200, 100, 300);
+        Double result = Aggregate.average(arrays);
+        assertThat(result, is(200.0));
+    }
 }
